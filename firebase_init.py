25fd517d1d3f27e9bee85aee00e4e0550061094c
Tirth <trth25102003@gmail.com>
2025-04-08 21:36:54 +0530

    kp

--- firebase_init.py
+++ firebase_init.py
@@ -1,7 +1,14 @@
 # firebase_init.py
-import firebase_admin
-from firebase_admin import credentials, firestore
+import os
+from firebase_admin import credentials, firestore, initialize_app
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("lodooooooo.json")
-firebase_admin.initialize_app(cred)
+load_dotenv()
+
+cred_path = os.getenv("GOOGLE_CREDENTIALS")
+if not cred_path:
+    raise Exception("GOOGLE_CREDENTIALS environment variable not set")
+
+cred = credentials.Certificate(cred_path)
+initialize_app(cred)
 db = firestore.client()
